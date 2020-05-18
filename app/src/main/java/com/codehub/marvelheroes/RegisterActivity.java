6bb5a63010248e59542a09ce403a307d481c4756
annakentistou <ann.kentistou@gmail.com>
2020-05-18 22:43:34 +0300

    v2 setOnClickListener to Register button

--- app/src/main/java/com/codehub/marvelheroes/RegisterActivity.java
+++ app/src/main/java/com/codehub/marvelheroes/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.codehub.marvelheroes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final Button register = findViewById(R.id.register_button);
+
+        register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
