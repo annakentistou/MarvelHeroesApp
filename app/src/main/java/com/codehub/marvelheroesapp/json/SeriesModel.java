24a249b141ff69bfbd188afb9d14c15a889d18c8
kongrillas <kongrillas@hotmail.com>
2020-06-01 18:44:01 +0300

    With series tab

---
+++ app/src/main/java/com/codehub/marvelheroesapp/json/SeriesModel.java
@@ -0,0 +1,52 @@
+package com.codehub.marvelheroesapp.json;
+
+import androidx.annotation.Nullable;
+
+public class SeriesModel {
+    private String title;
+    private String description;
+    private ImageModel thumbnail;
+    private CreatorsModel creators;
+
+    @Nullable
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Nullable
+
+    public String getDescription() {
+        return description;
+    }
+
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Nullable
+
+    public ImageModel getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(ImageModel thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    @Nullable
+
+    public CreatorsModel getCreators() {
+        return creators;
+    }
+
+    public void setCreators(CreatorsModel creators) {
+        this.creators = creators;
+    }
+}
